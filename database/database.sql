b2bc0b205c623d587da4c440e0b8a07f50f3ca59
carry0987 <carry0987@users.noreply.github.com>
2020-04-26 22:24:26 +0800

    Update database.sql

--- database/database.sql
+++ database/database.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS template (
   tpl_name varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
   tpl_type varchar(4) COLLATE utf8mb4_unicode_ci NOT NULL,
   tpl_md5 varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
-  tpl_expire_time int(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  tpl_expire_time int(20) UNSIGNED NOT NULL,
   tpl_verhash varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`tpl_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
