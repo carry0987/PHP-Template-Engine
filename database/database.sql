b4ee5ebce1498e0c5ab418035134d78f4f9053d4
carry0987 <carry0987@users.noreply.github.com>
2021-08-17 21:18:48 +0800

    Update database.sql

--- database/database.sql
+++ database/database.sql
@@ -4,7 +4,7 @@ SET time_zone = "+00:00";
 /* create template */
 CREATE TABLE IF NOT EXISTS template (
   tpl_id int(5) UNSIGNED NOT NULL AUTO_INCREMENT,
-  tpl_path varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  tpl_path varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
   tpl_name varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   tpl_type varchar(4) COLLATE utf8mb4_unicode_ci NOT NULL,
   tpl_md5 varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
